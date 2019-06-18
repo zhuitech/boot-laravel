543629eace1c9c5b9c49e866a7d286ae9cbed73e
安德鲁 <andrew@zhuitech.com>
2019-06-19 02:30:42 +0800

    closure cretia

---
+++ src/Repositories/ClosureCriteria.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrew
+ * Date: 2018/3/8
+ * Time: 13:15
+ */
+
+namespace ZhuiTech\BootLaravel\Repositories;
+
+use Bosnadev\Repositories\Criteria\Criteria;
+use Illuminate\Database\Eloquent\Model;
+use Bosnadev\Repositories\Contracts\RepositoryInterface;
+
+/**
+ * Class SimpleCriteria
+ * @package ZhuiTech\BootLaravel\Repositories
+ */
+class SimpleCriteria extends Criteria
+{
+    protected $wheres, $orders, $limit, $or;
+
+    public function __construct($wheres, $orders = NULL, $limit = NULL, $or = false)
+    {
+        $this->wheres = $wheres;
+        $this->orders = $orders;
+        $this->limit = $limit;
+        $this->or = $or;
+    }
+
+    /**
+     * @param $model
+     * @param RepositoryInterface $repository
+     * @return mixed
+     */
+    public function apply($model, RepositoryInterface $repository)
+    {
+        // Where
+        foreach ($this->wheres as $field => $value) {
+            if (is_array($value)) {
+                foreach ($value as $operator => $search) {
+                    $model = (!$this->or)
+                        ? $model->where($field, $operator, $search)
+                        : $model->orWhere($field, $operator, $search);
+                }
+            } else {
+                $model = (!$this->or)
+                    ? $model->where($field, '=', $value)
+                    : $model->orWhere($field, '=', $value);
+            }
+        }
+
+        // Order
+        if (!empty($this->orders)){
+            foreach ($this->orders as $field => $order) {
+                $model = $model->orderBy($field, $order);
+            }
+        }
+
+        // Limit
+        if (!empty($this->limit)) {
+            $model = $model->take($this->limit);
+        }
+
+        return $model;
+    }
+}
