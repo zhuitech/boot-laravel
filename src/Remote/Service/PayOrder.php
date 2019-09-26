eee541eeef9542de778595922d3665b84b6822e6
安德鲁 <andrew@zhuitech.com>
2019-09-26 21:44:53 +0800

    PayOrder

--- src/Remote/Service/PayOrder.php
+++ src/Remote/Service/PayOrder.php
@@ -7,49 +7,47 @@ use ZhuiTech\BootLaravel\Helpers\RestClient;
 use ZhuiTech\BootLaravel\Remote\Model;
 
 /**
- * Class PayPayment
+ * Class PayOrder
  * @package ZhuiTech\BootLaravel\Remote\Service
  *
  * @property int $id
+ * @property int|null $status 状态：0=待支付，1=已支付，2=部分支付，3=已退款
  * @property int $user_id 用户ID
- * @property int $order_id 业务ID
- * @property string $order_type 业务类型
- * @property string $order_no 业务编号
- * @property string|null $out_trade_no 交易编号
+ * @property string $order_type 订单类型
+ * @property string $order_no 订单编号
  * @property int $order_amount 订单金额
- * @property int $pay_amount 支付金额
- * @property int $actual_amount 实际支付金额
  * @property string|null $subject 标题
  * @property string|null $body 描述
  * @property string|null $notify_url 异步通知
  * @property string|null $return_url 同步通知
- * @property int|null $status 状态：0=未支付，1=支付成功
- * @property string|null $channel 支付通道
- * @property string|null $method 支付方式
- * @property string|null $trade_no 交易流水号
- * @property string|null $buyer 交易用户
- * @property array|null $result 结果
  * @property string|null $paid_at 付款时间
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read mixed $paid_amount
  * 
  */
-class PayPayment extends Model
+class PayOrder extends Model
 {
+    protected $cache = false;
+
     const STATUS_WAIT = 0;
-    const STATUS_SUCCESS = 1;
+    const STATUS_PAID = 1;
+    const STATUS_PARTPAID = 2;
+    const STATUS_REFUND = 3;
 
     public static $statusOptions = [
         self::STATUS_WAIT => '待支付',
-        self::STATUS_SUCCESS => '已支付',
+        self::STATUS_PAID => '已支付',
+        self::STATUS_PARTPAID => '部分支付',
+        self::STATUS_REFUND => '已退款',
     ];
     
     protected $server = 'service';
-    protected $resource = 'api/svc/pay/payments';
+    protected $resource = 'api/svc/pay/orders';
 
     public function charge($channel, $method, $options = null)
     {
-        $result = RestClient::server($this->server)->post("api/svc/pay/payments/{$this->id}/charge", compact('channel', 'method', 'options'));
+        $result = RestClient::server($this->server)->post("api/svc/pay/orders/{$this->id}/charge", compact('channel', 'method', 'options'));
         if ($result['status'] === true) {
             return $result['data'];
         } else {
