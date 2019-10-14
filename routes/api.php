28da01b4dcbe78eb22c45fb78697e9946272379b
andrew.zhou <andrew.zhou@pixelplus.co>
2019-10-14 23:07:38 +0800

    service proxy

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Routing\Router;
  */
 Route::group(['prefix' => 'svc', 'namespace' => 'ZhuiTech\BootLaravel\Controllers'], function (Router $router) {
     $registry = [
-        ['get', 'logistics/regions/select'], ['get', 'logistics/regions'],
+        ['get', 'logistics/regions/select'], ['get', 'logistics/regions'], ['get', 'logistics/companies'],
         ['post', 'sms/verify'], ['post', 'sms/check'],
         ['get', 'cms/ads/slug/{slug}'],
         ['post', 'pay/notify/{channel}'], ['post', 'pay/return/{channel}/{method}'], ['post', 'pay/notify/{channel}/{method}'],
