be3eb2af50fa885a19318c7fc20c90227c6079fe
andrew.zhou <andrew.zhou@pixelplus.co>
2019-10-29 23:54:12 +0800

    echo service

---
+++ src/Services/EchoClient.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ZhuiTech\BootLaravel\Services;
+
+use ZhuiTech\BootLaravel\Helpers\RestClient;
+
+class EchoClient extends RestClient
+{
+    protected $server = 'echo';
+
+    public function request($path, $method = 'GET', $options = [])
+    {
+        $options += ['query' => []];
+        $options['query']['auth_key'] = env('ECHO_KEY');
+
+        return parent::request($path, $method, $options);
+    }
+}
---
+++ src/Services/EchoService.php
@@ -0,0 +1,37 @@
+<?php
+namespace ZhuiTech\BootLaravel\Services;
+
+use ZhuiTech\BootLaravel\Helpers\RestClient;
+
+class EchoService
+{
+    const CACHE_KEY_CHANNELS = 'echo:channels';
+
+    protected $appId;
+
+    public function __construct()
+    {
+        $this->appId = env('ECHO_APP_ID');
+    }
+
+    public function channels()
+    {
+        // 查询缓存
+        if ($data = \Cache::get(self::CACHE_KEY_CHANNELS)) {
+            return $data;
+        }
+
+        // 请求后端服务
+        $result = EchoClient::server()->get("apps/{$this->appId}/channels");
+
+        // 处理返回结果
+        if (!empty($result) && isset($result['channels'])) {
+            $data = $result['channels'];
+
+            // 设置缓存
+            \Cache::put(self::CACHE_KEY_CHANNELS, $data, 2);
+        }
+
+        return $data ?? null;
+    }
+}
