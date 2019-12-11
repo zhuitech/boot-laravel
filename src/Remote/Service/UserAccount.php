37c1e50eb93cffd6b430b3e54817c118458ba12e
安德鲁 <andrew@zhuitech.com>
2019-12-11 21:50:00 +0800

    helper

--- src/Remote/Service/UserAccount.php
+++ src/Remote/Service/UserAccount.php
@@ -6,6 +6,32 @@ use Illuminate\Support\Collection;
 use ZhuiTech\BootLaravel\Remote\Model;
 use ZhuiTech\BootLaravel\Remote\Transformers\UserAccountPublicTransformer;
 
+/**
+ * Class UserAccount
+ * @package ZhuiTech\BootLaravel\Remote\Service
+ * 
+ * @property int $id
+ * @property string|null $name 用户名
+ * @property string|null $email 邮箱
+ * @property string|null $mobile 手机号码
+ * @property string|null $nickname 昵称
+ * @property string|null $password 密码
+ * @property int $status 状态：0=禁用，1=正常
+ * @property string|null $avatar 头像
+ * @property string|null $sex 性别
+ * @property string|null $real_name 真实姓名
+ * @property string|null $birthday 生日
+ * @property string|null $description 简介
+ * @property string|null $country 国家
+ * @property string|null $province 省份
+ * @property string|null $city 城市
+ * @property string|null $currency 货币类型
+ * @property string|null $id_type 证件类型
+ * @property string|null $id_number 证件号码
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * 
+ */
 class UserAccount extends Model
 {
     protected $server = 'service';
--- src/helpers.php
+++ src/helpers.php
@@ -26,6 +26,41 @@ if (! function_exists('relative_path')) {
     }
 }
 
+if (! function_exists('local_path')) {
+    /**
+     * 获取本地路径
+     *
+     * @param $real_path
+     * @return mixed
+     */
+    function local_path($path, $disk = null)
+    {
+        return Storage::disk($disk)->path($path);
+    }
+}
+
+if (! function_exists('storage_url')) {
+    /**
+     * 获取存储文件URL
+     *
+     * @param $path
+     * @param null $disk
+     * @return string
+     */
+    function storage_url($path, $disk = null)
+    {
+        if (URL::isValidUrl($path)) {
+            return $path;
+        }
+
+        if (empty($path)) {
+            return null;
+        }
+
+        return Storage::disk($disk)->url($path);
+    }
+}
+
 if (! function_exists('magic_replace')) {
     /**
      * 替换魔法变量
@@ -142,28 +177,6 @@ if (! function_exists('morph_alias')) {
     }
 }
 
-if (! function_exists('storage_url')) {
-    /**
-     * 获取存储文件URL
-     * 
-     * @param $path
-     * @param null $disk
-     * @return string
-     */
-    function storage_url($path, $disk = null)
-    {
-        if (URL::isValidUrl($path)) {
-            return $path;
-        }
-
-        if (empty($path)) {
-            return null;
-        }
-
-        return Storage::disk($disk)->url($path);
-    }
-}
-
 if (!function_exists('unique_no')) {
     /**
      * 创建唯一编号
@@ -173,20 +186,7 @@ if (!function_exists('unique_no')) {
      */
     function unique_no($prefix = 'O')
     {
-        // 订单号码主体（YYYYMMDDHHIISSNNNNNNNN）
-        $order_id_main = date('Ymd') . rand(100000000,999999999);
-
-        // 订单号码主体长度
-        $order_id_len = strlen($order_id_main);
-        $order_id_sum = 0;
-
-        for($i=0; $i<$order_id_len; $i++){
-            $order_id_sum += (int)(substr($order_id_main, $i,1));
-        }
-
-        // 唯一订单号码（YYYYMMDDHHIISSNNNNNNNNCC）
-        $order_id = $order_id_main . str_pad((100 - $order_id_sum % 100) % 100,2,'0',STR_PAD_LEFT);
-        return $prefix.$order_id;
+        return $prefix . date('ymd') . substr(implode(NULL, array_map('ord', str_split(substr(uniqid(), 7, 13), 1))), 0, 8);
     }
 }
 
