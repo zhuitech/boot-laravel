e3fef901dd39a3da3680c0c3b8b2e113a576704b
安德鲁 <andrew@zhuitech.com>
2020-03-01 16:33:47 +0800

    helper

--- src/helpers.php
+++ src/helpers.php
@@ -244,14 +244,17 @@ if (!function_exists('is_version')) {
 }
 
 if (!function_exists('var_export_new')) {
-    function var_export_new($expression, $return=FALSE)
+    function var_export_new($expression, $return = FALSE)
     {
         $export = var_export($expression, TRUE);
         $export = preg_replace("/^([ ]*)(.*)/m", '$1$1$2', $export);
+
         $array = preg_split("/\r\n|\n|\r/", $export);
         $array = preg_replace(["/\s*array\s\($/", "/\)(,)?$/", "/\s=>\s$/"], [NULL, ']$1', ' => ['], $array);
-        $array = preg_replace("/\d+ => \[/", '[', $array);
+        $array = preg_replace("/\d+ =>/", '', $array);
+
         $export = join(PHP_EOL, array_filter(["["] + $array));
+
         if ((bool)$return) {
             return $export;
         } else {
