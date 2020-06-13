570a25b85b9dbf6e1942db800c2b1a5639f2235d
安德鲁 <andrew@zhuitech.com>
2020-06-13 13:50:26 +0800

    debug

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,7 @@
 Route::group(['prefix' => 'assets/svc', 'namespace' => 'ZhuiTech\BootLaravel\Controllers'], function () {
     Route::any('/{wildcard?}', 'ServiceProxyController@api')->where('wildcard', '.+');
 });
+
+Route::get('debug/errors', function () {
+	dd(config('boot-laravel.errors'));
+});
