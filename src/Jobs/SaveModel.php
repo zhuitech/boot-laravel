507abaf5645b59b4a18bb630cc185c50305c925d
安德鲁 <andrew@zhuitech.com>
2020-09-18 23:51:39 +0800

    jwt令牌无数据支持

--- src/Jobs/SaveModel.php
+++ src/Jobs/SaveModel.php
@@ -13,24 +13,34 @@ class SaveModel implements ShouldQueue
 {
 	use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-	/**
-	 * @var Model
-	 */
 	protected $class;
 
 	protected $attributes;
 
+	protected $model;
+
+	protected $changes;
+
 	public function __construct(Model $model)
 	{
-		$this->class = get_class($model);
-		$this->attributes = $model->getAttributes();
+		$this->changes = $model->getDirty();
+
+		// 新对象不能序列化，保存类名
+		if ($model->exists) {
+			$this->model = $model;
+		} else {
+			$this->class = get_class($model);
+		}
 	}
 
 	public function handle()
 	{
-		/* @var $model Model */
-		$model = new $this->class;
-		$model->setRawAttributes($this->attributes);
-		$model->save();
+		// 构建新对象
+		if (!$this->model && $this->class) {
+			$this->model = new $this->class;
+		}
+
+		$this->model->fill($this->changes);
+		$this->model->save();
 	}
 }
