5eeb9f8d92c5ff4d4bc6221bdfb0626efcc70d76
安德鲁 <andrew@zhuitech.com>
2020-11-18 23:28:49 +0800

    升级8.0

---
+++ src/Database/Schema.php
@@ -0,0 +1,52 @@
+<?php
+namespace ZhuiTech\BootLaravel\Database;
+
+use \Illuminate\Support\Facades\Facade;
+
+/**
+ * Class Schema
+ * @package Jialeo\LaravelSchemaExtend
+ * @method static create(string $table_name, \Closure $callback)
+ */
+class Schema extends Facade
+{
+    /**
+     * Get a schema builder instance for a connection.
+     *
+     * @param  string  $name
+     * @return \Illuminate\Database\Schema\Builder
+     */
+    public static function connection($name)
+    {
+        $connection = static::$app['db']->connection($name);
+        return static::useCustomGrammar($connection);
+    }
+
+    /**
+     * Get a schema builder instance for the default connection.
+     *
+     * @return \Illuminate\Database\Schema\Builder
+     */
+    protected static function getFacadeAccessor()
+    {
+        $connection = static::$app['db']->connection();
+        return static::useCustomGrammar($connection);
+    }
+
+
+    /**
+     * lead the system to load custom Grammar
+     * @param $connection
+     */
+    protected static function useCustomGrammar($connection)
+    {
+        // just for MySqlGrammar
+        if (get_class($connection) === 'Illuminate\Database\MySqlConnection') {
+            $MySqlGrammar = $connection->withTablePrefix(new MySqlGrammar);
+            $connection->setSchemaGrammar($MySqlGrammar);
+        }
+
+        return $connection->getSchemaBuilder();
+    }
+
+}
